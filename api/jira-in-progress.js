d88d3156ce3db806b003b76661407011616c954f
bartosz.bossy <bartosz.bossy@auctane.com>
2026-02-27 11:34:11 +0100

    zmienne loklane

--- api/jira-in-progress.js
+++ api/jira-in-progress.js
@@ -12,7 +12,6 @@ export default async function handler(req, res) {
   }
 
   if (req.method !== 'GET') {
-      console.log('test');
     return res.status(405).json({ error: 'Method not allowed' });
   }
 
